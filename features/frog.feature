9936ba6af96aef8253dcac9229b1f629422f15d4
Dave <d4park@gmail.com>
2014-11-06 17:09:34 -0600

    First scenario passing.  Rewrote the step definitions for the current expect usage of rspec.

--- features/frog.feature
+++ features/frog.feature
@@ -9,8 +9,7 @@ Feature: Frog features
 	Scenario: Random number generation test
 		Given a frog
 		Then the frog's random value should be an integer
-		And the frog's random value should be greater than or equal to 1
-		And the frog's random value should be less than or equal to 100000
+		And the frog's random value should be between 1 and 100000
 		
 	Scenario Outline: Method returning expected values
 		Given a frog
@@ -24,10 +23,10 @@ Feature: Frog features
 		|2		|21			|2	|
 		
 	Scenario Outline: Checking the worst case scenario is still within O(N) of time and O(X) of space
-		Given a frog jumping across the river
+		Given a frog
 		And the number of leaves is <leaves>
 		And the leaves do not fall in a favorable pattern
-		Then the method should iterate an equal number of times as <leaves>
+		Then the earliest the frog can get across the river is in <leaves> minutes
 		And the length of the frog jump array should be equal or less than <leaves>
 		
 		Examples:
--- features/step_definitions/frog_steps.rb
+++ features/step_definitions/frog_steps.rb
@@ -7,47 +7,33 @@ end
 Then(/^the frog's random value should be an integer$/) do
   leaves = @frog.num_leaves.is_a? Integer
   river = @frog.river_width.is_a? Integer
-  (leaves && river).should == true #If either are false, this test fails.
+  expect(leaves && river).to eq(true) #If either are false, this test fails.
 end
 
-Then(/^the frog's random value should be greater than or equal to (\d+)$/) do |min|
-  @frog.num_leaves.should >= min
-  @frog.river_width.should >= min
-end
-
-Then(/^the frog's random value should be less than or equal to (\d+)$/) do |max|
-  @frog.num_leaves.should <= max
-  @frog.river_width.should <= max
+Then(/^the frog's random value should be between (\d+) and (\d+)$/) do |min, max|
+  expect(@frog.num_leaves).to be_between(min.to_i, max.to_i).exclusive
+  expect(@frog.river_width).to be_between(min.to_i, max.to_i).exclusive
 end
 
 Given(/^the number of leaves is (\d+)$/) do |leaves|
-  @frog.leaves = leaves 
+  @frog.num_leaves = leaves.to_i 
 end
 
 #This is a poorly made step - any pattern longer than 9 leaves will not work with this formula.  If separated by commas, this regular expression fails,
 #And if a regular expression is written to incorporate commas, it will only take the last number (EOL issue) or the step will have to
 #be customized for the amount of leaves in the pattern, and redundant steps will occur.
 Given(/^the leaves fall in pattern (\d+)$/) do |pattern|
-  binding.pry #binding here to doublecheck if <pattern> is a string or an int
-  @frog.pattern = pattern.to_s.split('').map{|e| e.to_i} #Takes an integer and splits it using string functions into an array of single digits
+  @frog.pattern = pattern.split('').map{|e| e.to_i} #Takes an integer and splits it using string functions into an array of single digits
 end
 
 Then(/^the earliest the frog can get across the river is in (\d+) minutes$/) do |mins|
-  @frog.time.should == mins
-end
-
-Given(/^a frog jumping across the river$/) do
-  @frog = Frog.create!
+  expect(@frog.time).to eq(mins.to_i)
 end
 
 Given(/^the leaves do not fall in a favorable pattern$/) do
   @frog.generate_bad_pattern
 end
 
-Then(/^the method should iterate an equal number of times as (\d+)$/) do |mins|
-  @frog.time.should == mins
-end
-
 Then(/^the length of the frog jump array should be equal or less than (\d+)$/) do |jumps|
-  @frog.steps.length.should <= jumps
+  expect(@frog.steps.length).to be <= jumps
 end
