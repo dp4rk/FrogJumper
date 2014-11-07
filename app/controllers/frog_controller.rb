40e2f0c68dc139c47a02794676c28750c24a1e98
Dave <d4park@gmail.com>
2014-11-07 12:30:11 -0600

    Finished the project.  Front-end interface is up and running.  Removed any unnecessary files.

--- app/controllers/frog_controller.rb
+++ app/controllers/frog_controller.rb
@@ -3,21 +3,38 @@ class FrogController < ApplicationController
     
     @frog = Frog.new
 
-    if params[:width]
-      @frog.river_width = params[:width].to_i
+    #If all fields are left blank, just randomize it!
+    if params[:width].blank? && params[:pattern].blank? && params[:leaves].blank?
+      @frog.river_width = 1 + rand(100000)
+      @pattern = @frog.generate_crossing_pattern(@frog.river_width)
     
     else
-      @frog.river_width = 1 + rand(100000)
-    end
+      #If only the width is blank, randomize it.  
+      if params[:width].blank?
+        @frog.river_width = 1 + rand(100000)
+      
+      else
+        @frog.river_width = params[:width].to_i
+      end
+      
+      #Use the river width to generate the crossing pattern if no pattern or leaf number has been entered
+      if params[:pattern].blank? && params[:leaves].blank?
+        @pattern = @frog.generate_crossing_pattern(@frog.river_width)
+      
+      #Else one of the two fields has been submitted
+      else
+        #If the pattern is blank, generate the crossing pattern on the number of leaves, N
+        if params[:pattern].blank?
+          @pattern = @frog.generate_crossing_pattern(params[:leaves].to_i)
         
-    if params[:pattern].blank? == false
-      @pattern = params[:pattern].split(',').map{|e| e.to_i} #Splits the input pattern on commas and separates them into ints
-    
-    elsif params[:leaves]
-      @pattern = @frog.generate_crossing_pattern(params[:leaves].to_i)
+        #If the pattern is not blank, use the pattern submitted
+        else
+          @pattern = params[:pattern].split(',').map{|e| e.to_i} #Splits the input pattern on commas and separates them into ints
+        end
+      end #endif
     end #endif
-          
-    @time = @frog.time_to_cross_river(@pattern)
+            
+    @time = @frog.time_to_cross_river(@pattern) #This is the final function.  Input is an array of integers.
   end #end new
   
 end#end frog_controller
--- test/helpers/frog_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class FrogHelperTest < ActionView::TestCase
-end
